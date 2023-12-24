50a45dba2152316c9fad53b0bc76747c6717b31e
Christian Moore <cmoore1776@icloud.com>
2023-12-24 08:05:23 -0500

    update flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,11 @@
 cd ffmpeg-windows-build-helpers
 
 ./cross_compile_ffmpeg.sh \
-  --build-amd-amf=y \
-  --build-dependencies=y \
-  --build-dvbtee=n \
   --build-ffmpeg-shared=y \
   --build-ffmpeg-static=y \
   --build-intel-qsv=y \
   --build-ismindex=n \
-  --build-libmxf=n \
-  --build-lsw=n \
-  --build-mp4box=n \
-  --build-mplayer=n \
-  --build-svt=y \
-  --build-vlc=n \
-  --build-x264-with-libav=n \
+  --build-svt-hevc=y \
   --compiler-flavors=win64 \
   --disable-nonfree=n \
   --enable-gpl=y \
